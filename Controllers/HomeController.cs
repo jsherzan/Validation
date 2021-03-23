5735733a63e03563860967dcfa0216885bba10d6
jsherzan <77797866+jsherzan@users.noreply.github.com>
2021-03-23 00:42:07 -0500

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Validation.Models;
+
+namespace Validation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            var model = new RegistraionModel();
+            model.ContactPreferences = new List<Microsoft.AspNetCore.Mvc.Rendering.SelectListItem>();
+            model.ContactPreferences.Add(new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem() { Text = "Phone", Value = "1", Selected = false });
+            model.ContactPreferences.Add(new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem() { Text = "Email", Value = "2", Selected = false });
+            model.ContactPreferences.Add(new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem() { Text = "Postal", Value = "3", Selected = false });
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Registration(RegistraionModel model)
+        {
+            return RedirectToAction("ViewFormData", model);
+        }
+        public IActionResult ViewFormData(RegistraionModel model)
+        {
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/RegistraionModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Validation.Models
+{
+    public class RegistraionModel
+    {
+        [StringLength(60, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 60 characters.")]
+        [Required(ErrorMessage = "Name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Address is required.")]
+        public string Address { get; set; }
+
+        [RegularExpression(@"^[1-9]\d{2}-\d{3}-\d{4}", ErrorMessage = "Please enter a valid phone number in ###-###-#### format.")]
+        [Required(ErrorMessage = "Phone Number is required.")]
+        public string PhoneNumber { get; set; }
+
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter a valid email.")]
+        [Required(ErrorMessage = "Email is required.")]
+        public string Email { get; set; }
+
+        [Range(13,100, ErrorMessage = "Please enter a valid age between 13 and 100.")]
+        [Required(ErrorMessage = "Age is required.")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Contact Preference is required.")]
+        public int ContactPreference { get; set; }
+
+        public List<SelectListItem> ContactPreferences { get; set; }
+
+    }
+}
